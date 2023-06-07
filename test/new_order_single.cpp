8a0c405387bb46cdeb11494afeb838b11bfaac0c
thraneh <thraneh@gmail.com>
2023-06-07 05:19:01 +0200

    update

--- test/new_order_single.cpp
+++ test/new_order_single.cpp
@@ -20,15 +20,17 @@ using namespace cme_ilink;
 
 using namespace roq;
 
+using value_type = NewOrderSingle514;
+
 namespace {
 template <typename T>
 auto encode(auto &buffer, auto &message_header, T &value) {
   value.sbeRewind();  // note!
   message_header.wrap(reinterpret_cast<char *>(std::data(buffer)), 0, 0, std::size(buffer))
-      .blockLength(NewOrderSingle514::sbeBlockLength())
-      .templateId(NewOrderSingle514::sbeTemplateId())
-      .schemaId(NewOrderSingle514::sbeSchemaId())
-      .version(NewOrderSingle514::sbeSchemaVersion());
+      .blockLength(value_type::sbeBlockLength())
+      .templateId(value_type::sbeTemplateId())
+      .schemaId(value_type::sbeSchemaId())
+      .version(value_type::sbeSchemaVersion());
   value.wrapForEncode(reinterpret_cast<char *>(std::data(buffer)), message_header.encodedLength(), std::size(buffer));
   value.sbeRewind();  // note!
   auto length = value.computeLength();
@@ -39,7 +41,7 @@ auto encode(auto &buffer, auto &message_header, T &value) {
 TEST_CASE("simple", "[new_order_single]") {
   std::vector<std::byte> buffer(4096);
   // MessageHeader message_header;
-  NewOrderSingle514 new_order_single;
+  value_type new_order_single;
   auto &nso = new_order_single.wrapAndApplyHeader(reinterpret_cast<char *>(std::data(buffer)), 0, std::size(buffer));
   nso.price().mantissa(77);
   nso.orderQty(123)
@@ -71,14 +73,10 @@ TEST_CASE("simple", "[new_order_single]") {
   price.mantissa(mantissa);
   auto message = encode(buffer, message_header, new_order_single);
   */
-  std::span message{std::data(buffer), NewOrderSingle514::sbeBlockAndHeaderLength()};
+  std::span message{std::data(buffer), value_type::sbeBlockAndHeaderLength()};
   fmt::print(stderr, "{}\n"sv, debug::hex::Message{message});
   fmt::print(
-      stderr,
-      "{} {} {}\n"sv,
-      std::size(message),
-      NewOrderSingle514::sbeBlockAndHeaderLength(),
-      NewOrderSingle514::sbeBlockLength());
+      stderr, "{} {} {}\n"sv, std::size(message), value_type::sbeBlockAndHeaderLength(), value_type::sbeBlockLength());
   auto expected =
       "\x7c\x00"  // block length
       "\x02\x02"  // template id (514)
--- test/order_mass_action_request.cpp
+++ test/order_mass_action_request.cpp
@@ -12,6 +12,8 @@
 
 #include "roq/debug/hex/message.hpp"
 
+#include "roq/cme/ilink/order_mass_action_request.hpp"
+
 using namespace std::literals;
 
 using namespace Catch::literals;
@@ -19,7 +21,38 @@ using namespace Catch::literals;
 using namespace cme_ilink;
 
 using namespace roq;
-// using namespace roq::cme;
+using namespace roq::cme;
+
+namespace {
+using value_type = cme_ilink::OrderMassActionRequest529;
+}
+
+TEST_CASE("order_mass_action_request_simple", "[order_mass_action_request]") {
+  std::vector<std::byte> buffer(4096);
+  auto order_mass_action_request = ilink::OrderMassActionRequest{
+      .party_details_list_req_id = {},  // note!
+      .order_request_id = 1,
+      .manual_order_indicator = cme_ilink::ManualOrdIndReq::Automated,
+      .seq_num = 1,
+      .sender_id = "A1"sv,
+      .sending_time_epoch = 1ns,
+      .security_group = {},
+      .location = "UK"sv,
+      .security_id = {},
+      .mass_action_scope = cme_ilink::MassActionScope::MarketSegmentID,
+      .market_segment_id = 84,
+      .mass_cancel_request_type = cme_ilink::MassCxlReqTyp::Account,
+      .side = cme_ilink::SideNULL::NULL_VALUE,
+      .ord_type = cme_ilink::MassActionOrdTyp::NULL_VALUE,
+      .time_in_force = cme_ilink::MassCancelTIF::NULL_VALUE,
+      .liquidity_flag = cme_ilink::BooleanNULL::NULL_VALUE,
+      .orig_order_user = {},
+  };
+  auto message = order_mass_action_request.encode(buffer);
+  fmt::print(stderr, "{}\n"sv, debug::hex::Message{message});
+  fmt::print(stderr, "{}\n"sv, debug::hex::Message{message.subspan(8 + 68, 1)});
+  fmt::print(stderr, "{}\n"sv, order_mass_action_request.ord_type);
+}
 
 TEST_CASE("simple", "[order_mass_action_request]") {
   auto message =
@@ -47,4 +80,30 @@ TEST_CASE("simple", "[order_mass_action_request]") {
       "\xff"                                      // time in force (null)
       "\x00"                                      // liquidity flag
       "\x00\x00\x00\x00\x00\x00\x00\x00"sv;       // orig order user
+  //\x5b\x00\xfe\xca\x4f\x00\x11\x02\x08\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x41\x31\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xcb\x8c\x9b\x71\xc0\x40\x66\x17\x00\x00\x00\x00\x00\x00\x55\x4b\x00\x00\x00\xff\xff\xff\x7f\x09\x54\x65\xff\x30\xff\xff\x00\x00\x00\x00\x00\x00\x00\x00
+  auto msg =
+      "\x5b\x00"
+      "\xfe\xca"
+      "\x4f\x00"
+      "\x11\x02"
+      "\x08\x00"
+      "\x08\x00"
+      "\x00\x00\x00\x00\x00\x00\x00\x00"
+      "\x01\x00\x00\x00\x00\x00\x00\x00"
+      "\x00"
+      "\x02\x00\x00\x00"
+      "\x41\x31\x00\x00\x00\x00\x00\x00\x00\x00"
+      "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+      "\xcb\x8c\x9b\x71\xc0\x40\x66\x17"
+      "\x00\x00\x00\x00\x00\x00"
+      "\x55\x4b\x00\x00\x00"
+      "\xff\xff\xff\x7f"
+      "\x09"
+      "\x54"
+      "\x65"
+      "\xff"
+      "\x30"
+      "\xff"
+      "\xff"
+      "\x00\x00\x00\x00\x00\x00\x00\x00"sv;
 }
