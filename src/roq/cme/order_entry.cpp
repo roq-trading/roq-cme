8d4661d6f9cfdaa644a9acaf91339bfdd3164d90
thraneh <thraneh@gmail.com>
2023-06-13 05:27:22 +0200

    update

--- src/roq/cme/order_entry.cpp
+++ src/roq/cme/order_entry.cpp
@@ -740,8 +740,8 @@ void OrderEntry::send_new_order_single(
             .ord_type = ord_type,
             .time_in_force = time_in_force,
             .manual_order_indicator = MANUAL_ORDER_INDICATOR,
-            .exec_inst = {},  // XXX
-            .execution_mode = cme_ilink::ExecMode::Passive,
+            .exec_inst = {},                                               // XXX
+            .execution_mode = static_cast<cme_ilink::ExecMode::Value>(0),  // cme_ilink::ExecMode::Passive,
             .liquidity_flag = {},
             .managed_order = {},
             .short_sale_type = cme_ilink::ShortSaleType::LongSell,
