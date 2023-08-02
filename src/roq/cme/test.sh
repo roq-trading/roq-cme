f48ef01fe058cc7938315f76b87f6eaeeb15322e
thraneh <thraneh@gmail.com>
2023-08-02 14:18:16 +0200

    script

--- src/roq/cme/test.sh
+++ src/roq/cme/test.sh
@@ -47,3 +47,27 @@ $PREFIX "./roq-cme" \
   --ilink_config_file="$ILINK_CONFIG_FILE" \
   --secdef_config_file="$SECDEF_CONFIG_FILE" \
   $@
+
+# $PREFIX "./roq-cme" \
+#   --name=cme \
+#   --event_log_symlink=true \
+#   --multicast_channel_ids=344 \
+#   --multicast_local_interface=172.19.0.1 \
+#   --multicast_buffer_depth=2 \
+#   --event_log_iso_week=false
+#   --enable_market_by_order=true
+#   --test_mbp_to_mbo_clear_price_level=true
+#   --ilink_firm_id=ROQ
+#   --ilink_port=65301
+#   --ilink_market_segment_ids=84
+#   --ilink_location=UK
+#   --config_file=$HOME/etc/cme/config.toml \
+#   --client_listen_address=$HOME/run/cme.sock \
+#   --service_listen_address=$HOME/run/cme-service.sock \
+#   --cache_dir=$HOME/var/lib/roq/cache \
+#   --event_log_dir=$HOME/var/lib/roq/data \
+#   --multicast_config_file=$SHARE/config.xml \
+#   --ilink_config_file=$SHARE/MSGW_Config.xml \
+#   --auth_keys_file=/home/quincy/keys.json \
+#   --secdef_config_file=$HOME/secdef.dat \
+#   $@
