d810dd1e54efb85056fea17331d3b3f049539fef
thraneh <thraneh@gmail.com>
2023-08-15 06:08:21 +0200

    update

---
+++ scripts/download.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+USER="cmeconfig"
+PASSWORD="G3t(0nnect3d"
+
+URL="sftp://sftpng.cmegroup.com"
+
+lftp \
+  -u "$USER,$PASSWORD" \
+  -e "set ssl:verify-certificate no;" \
+  -e "set xfer:clobber on;" \
+  $URL << EOM
+lcd share/prod
+cd /MSGW/Production/Configuration
+mget *.xml
+cd /SBEFix/Production/Configuration
+get config.xml
+lcd ../test
+cd /MSGW/Cert/Configuration
+mget *.xml
+cd /SBEFix/Cert/Configuration
+get config.xml
+lcd ../cert
+# maybe...
+cd /MSGW/Cert/Configuration
+mget *.xml
+cd /SBEFix/CertAutoCertPlus/Configuration
+get config.xml
+EOM
