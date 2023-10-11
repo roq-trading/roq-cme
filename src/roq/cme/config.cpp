dd7da659a70fb0b43e0fb4c7a4fb5839046b9e53
thraneh <thraneh@gmail.com>
2023-10-11 11:03:45 +0200

    GatewaySettings.oms_cancel_all_orders to communicate what filters are supported (#414)

--- src/roq/cme/config.cpp
+++ src/roq/cme/config.cpp
@@ -25,6 +25,7 @@ auto const SUPPORTS = Mask{
 auto const MBP_MAX_DEPTH = 10;
 auto const MBP_ALLOW_REMOVE_NON_EXISTING = true;
 auto const OMS_REQUEST_ID_TYPE = RequestIdType::BASE64;
+auto const OMS_CANCEL_ALL_ORDERS = Mask<CancelAllOrdersFilter>{};
 }  // namespace
 
 // === HELPERS ===
@@ -42,6 +43,7 @@ auto create_gateway_settings(auto &settings) -> GatewaySettings {
       .oms_download_has_state = {},
       .oms_download_has_routing_id = {},
       .oms_request_id_type = OMS_REQUEST_ID_TYPE,
+      .oms_cancel_all_orders = OMS_CANCEL_ALL_ORDERS,
   };
 }
 }  // namespace
