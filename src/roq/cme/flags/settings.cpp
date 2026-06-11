820446a674bf82b1f9c6496225865a5bf87881d7
thraneh <thraneh@gmail.com>
2026-06-11 06:34:08 +0000

    send git-hash to license server

--- src/roq/cme/flags/settings.cpp
+++ src/roq/cme/flags/settings.cpp
@@ -13,8 +13,8 @@ namespace cme {
 namespace flags {
 
 Settings::Settings(args::Parser const &args)
-    : server::flags::Settings{args, ROQ_PACKAGE_NAME, ROQ_BUILD_NUMBER}, flags::Flags{flags::Flags::create()}, misc{flags::Misc::create()},
-      multicast{flags::Multicast::create()}, ilink{flags::iLink::create()}, test{flags::Test::create()} {
+    : server::flags::Settings{args, ROQ_PACKAGE_NAME, ROQ_BUILD_NUMBER, ROQ_GIT_DESCRIBE_HASH, {}}, flags::Flags{flags::Flags::create()},
+      misc{flags::Misc::create()}, multicast{flags::Multicast::create()}, ilink{flags::iLink::create()}, test{flags::Test::create()} {
   log::info("settings={}"sv, *this);
 }
 
